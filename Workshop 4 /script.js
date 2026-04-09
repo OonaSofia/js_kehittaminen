f841e5aa3035182dd20d6d46cd1a7e90980c1fa2
Oona Sofia <Oona.S.Karkkainen@student.laurea.fi>
2026-04-09 18:09:45 +0300

    workshop 4

--- Workshop 4 /script.js
+++ Workshop 4 /script.js
@@ -1,10 +1,15 @@
-// Tehtävä 1 
+// Tehtävä 1
 function changeHeading() {
-let otsikko = document.querySelector("#Tehtävä1");
-otsikko.textContent = "Muokattu otsikko!";
+  let otsikko = document.querySelector("#Tehtävä1");
+  otsikko.textContent = "Muokattu otsikko!";
 }
-function secondButton(){
-let otsikkotoinen = document.querySelector("#Tehtävä2");
-otsikkotoinen.style.fontWeight = "300";
-otsikkotoinen.style.color= "blue";
+function secondButton() {
+  let otsikkotoinen = document.querySelector("#Tehtävä2");
+  otsikkotoinen.style.fontWeight = "300";
+  otsikkotoinen.style.color = "blue";
+  otsikkotoinen.style.backgroundColor = "red";
+}
+function thirdbutton() {
+  let kolmasotsikko = document.querySelector("#lorem2");
+  kolmasotsikko.textContent = "Tänään aurinko paistaa kirkkaasti ja keväinen sää houkuttelee ulos kävelylle. Puissa näkyy jo uusia silmuja ja ilma tuntuu raikkaalta.";
 }
