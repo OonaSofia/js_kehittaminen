12f7bdd9fd5258b15ef54e952f60ba7777926cdb
Oona Sofia <Oona.S.Karkkainen@student.laurea.fi>
2026-04-09 17:49:59 +0300

    workshop 4

---
+++ Workshop 4 /script.js
@@ -0,0 +1,10 @@
+// Tehtävä 1 
+function changeHeading() {
+let otsikko = document.querySelector("#Tehtävä1");
+otsikko.textContent = "Muokattu otsikko!";
+}
+function secondButton(){
+let otsikkotoinen = document.querySelector("#Tehtävä2");
+otsikkotoinen.style.fontWeight = "300";
+otsikkotoinen.style.color= "blue";
+}
